42845a99875ef6f50340d5ca99dcb21172603c7c
Sudhanshu-shukl <shuklasudhanshu230405@gmail.com>
2025-08-15 20:22:32 +0530

    8/15/25

--- 02 Bit Manipulation/Power_Set.py
+++ 02 Bit Manipulation/Power_Set.py
@@ -4,7 +4,7 @@ subsets = 1<<n
 res = []
 for i in range(subsets): # 0 1 2 3 4 5 6 7
     arr1 = []
-    for j in range(n):
+    for j in range(n): # 0 1 2
         if i & 1<<j:
             arr1.append(arr[j])
     res.append(arr1)
